a9036d3b213669d86a0aa45777eb2a42cb26fb76
Marius Smarandescu <marius.smarandescu@randstaddigital.com>
2023-12-18 08:59:20 +0200

    task1: Exemplificati in main ordinea apelarii constructorilor si destructorilor pentru clasele de baza si derivate

--- main.cpp
+++ main.cpp
@@ -7,7 +7,21 @@
 
 int main()
 {
+    {   //apelare constructor clasa Automobil:
+        Automobil a("benzina", "berlina", "integrala", 220, "full", "4Y1SL65848Z411439");
+        cout << "    obj class Automobil was created" << endl; 
+    }cout << "end of scope obj Automobil" << endl << endl; 
     
+    {   //apelare constructor clasa Audi:
+        Audi audi("benzina", "berlina", "integrala", 220, "full", "4Y1SL65848Z411439", "Audi", "A4");
+        cout << "   obj class Audi was created" << endl; 
+    }cout << "end of scope obj Audi" << endl << endl; 
+
+    {   //apelare constructor clasa Mercedes:
+        Mercedes m("motorina", "coupe", "2x4", 180, "full", "5Z1BD65348Z412343", "Pneumatica");
+        cout << "   obj class Mercedes was created" << endl; 
+    }cout << "end of scope obj Mercedes" << endl << endl; 
+
 
     return 0;
 }
